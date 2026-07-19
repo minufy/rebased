f1f6f06bb11a608d32617e5c77f8cd0d44fd9b07
minufy <seomeenu@proton.me>
2026-07-20 02:22:56 +0900

    update particle

--- objects/particle.lua
+++ objects/particle.lua
@@ -1,5 +1,8 @@
 local Particle = Object:extend()
-SetType(Particle, "particle")
+
+local move_damp = 0.1
+local size_damp = 0.07
+local size_thresh = 0.5
 
 function Particle:new(x, y, mx, my, size, color)
     self.x = x
@@ -24,11 +27,11 @@ function Particle:update(dt)
     self.x = self.x+self.mx*dt
     self.y = self.y+self.my*dt
 
-    self.mx = self.mx+(0-self.mx)*0.1*dt
-    self.my = self.my+(0-self.my)*0.1*dt
+    self.mx = self.mx-self.mx*move_damp*dt
+    self.my = self.my-self.my*move_damp*dt
     
-    self.size = self.size+(0-self.size)*0.07*dt
-    if self.size < 0.5 then
+    self.size = self.size-self.size*size_damp*dt
+    if self.size < size_thresh then
         self.remove = true
     end
 end
