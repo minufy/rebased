465d270243c47388f7efdf4eb4b5dbc97206558f
minufy <seomeenu@proton.me>
2026-07-23 03:08:19 +0900

    settype

--- main.lua
+++ main.lua
@@ -17,6 +17,8 @@ require("scenes.sm")
 require("settings")
 Particle = require("objects.particle")
 SparkParticle = require("objects.spark_particle")
+SetType(Particle, "particle")
+SetType(SparkParticle, "particle")
 
 function love.load()
     LogFont = love.graphics.newFont(20)
