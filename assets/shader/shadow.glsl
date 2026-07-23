c167b77f467a762e98a1fdaa00a358ff27cb7642
minufy <seomeenu@proton.me>
2026-07-23 19:01:00 +0900

    update shadow 2

--- assets/shader/shadow.glsl
+++ assets/shader/shadow.glsl
@@ -1,6 +1,6 @@
-uniform vec4 Color;
+uniform float alpha;
 
 vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords){
     vec4 pixel = Texel(tex, texture_coords);
-    return vec4(Color.x, Color.y, Color.z, pixel.a*Color.w);
+    return vec4(0, 0, 0, pixel.a*alpha);
 }
